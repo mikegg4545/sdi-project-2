18bf4f156d139eeb25edf38739dc3a671b5f27b9
mikegg4545 <42780320+mikegg4545@users.noreply.github.com>
2026-03-08 17:40:53 -0600

    Add fetch and render coins

--- src/pages/CoinsPage.jsx
+++ src/pages/CoinsPage.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+
 function CoinsPage() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinlore.net/api/tickers/?limit=10")
+      .then((res) => res.json())
+      .then((data) => {
+        setCoins(data.data);
+      });
+  }, []);
+
   return (
     <section>
       <h1>Coins</h1>
-      <p>Coin list will appear here.</p>
+      {/* <p>Coin list will appear here.</p> */}
+      {coins.map((coin) => (
+        <div key={coin.id}>
+          <h3>{coin.name}</h3>
+          <p>Price: ${coin.price_usd}</p>
+        </div>
+      ))}
     </section>
   );
 }
