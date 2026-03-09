6c7bc82920970740099f3829c10d50cce9f860f9
mikegg4545 <42780320+mikegg4545@users.noreply.github.com>
2026-03-08 22:46:31 -0600

    Add sorted coins

--- src/pages/CoinsPage.jsx
+++ src/pages/CoinsPage.jsx
@@ -4,6 +4,7 @@ import CoinCard from "../components/CoinCard";
 function CoinsPage() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("rank");
 
   useEffect(() => {
     fetch("https://api.coinlore.net/api/tickers/?limit=10")
@@ -17,9 +18,29 @@ function CoinsPage() {
     coin.name.toLowerCase().includes(search.toLowerCase()),
   );
 
+  let sortedCoins = [...filteredCoins];
+
+  if (sort === "price") {
+    sortedCoins.sort((a, b) => b.price_usd - a.price_usd);
+  }
+
+  if (sort === "name") {
+    sortedCoins.sort((a, b) => b.name.localeCompare(b.name));
+  }
+
+  if (sort === "rank") {
+    sortedCoins.sort((a, b) => b.rank - a.rank);
+  }
+
   return (
     <section>
       <h1>Coins</h1>
+      <select value={sort} onChange={(e) => setSort(e.target.value)}>
+        <option value="rank">Rank</option>
+        <option value="price">Price</option>
+        <option value="name">Name</option>
+      </select>
+
       {/* <p>Coin list will appear here.</p> */}
       <input
         type="text"
@@ -27,7 +48,7 @@ function CoinsPage() {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      {filteredCoins.map((coin) => (
+      {sortedCoins.map((coin) => (
         <CoinCard key={coin.id} coin={coin} />
       ))}
     </section>
