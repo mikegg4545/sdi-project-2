ca1ba3dd6e85f59ffe989ff6561fac18d3175f57
mikegg4545 <42780320+mikegg4545@users.noreply.github.com>
2026-03-09 12:30:06 -0600

    Add local storage, update styling/add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { WatchlistContext } from "../context/WatchlistContext";
+import logo from "../assets/cryptotracker logo.png";
 
 function Navbar() {
   const { watchlist } = useContext(WatchlistContext);
@@ -8,18 +9,34 @@ function Navbar() {
   return (
     <nav className="bg-gray-800 border-b border-gray-700">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
-        <h2 className="text-xl font-bold">Crypto Tracker</h2>
+        {/* <h2 className="text-xl font-bold">Crypto Tracker</h2> */}
+        <Link to="/" className="flex items-center gap-2">
+          <img
+            src={logo}
+            alt="Crypto Tracker logo"
+            className="w-auto h-12 transition-transform hover:scale-105"
+          />
+        </Link>
 
         <div className="flex gap-6 text-sm">
-          <Link to="/" className="hover:text-blue-400 transition">
+          <Link
+            to="/"
+            className="hover:text-blue-400 transition-transform hover:scale-105"
+          >
             Home
           </Link>
 
-          <Link to="/coins" className="hover:text-blue-400 transition">
+          <Link
+            to="/coins"
+            className="hover:text-blue-400 transition-transform hover:scale-105"
+          >
             Coins
           </Link>
 
-          <Link to="/watchlist" className="hover:text-blue-400 transition">
+          <Link
+            to="/watchlist"
+            className="hover:text-blue-400 transition-transform hover:scale-105"
+          >
             Watchlist: {watchlist.length}
           </Link>
         </div>
--- src/context/WatchlistContext.jsx
+++ src/context/WatchlistContext.jsx
@@ -1,10 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 // create watchlist context
 export const WatchlistContext = createContext();
 
 function WatchlistProvider({ children }) {
-  const [watchlist, setWatchlist] = useState([]);
+  const [watchlist, setWatchlist] = useState(() => {
+    // check local storage for existing watchlist
+    const saved = localStorage.getItem("watchlist");
+    // if so, convert to array, use as initial state
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  // if watchlist changes, update local storage
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+  }, [watchlist]);
 
   function addCoin(coin) {
     // check if coin on list already
